169cacb9595bd134175990f1a58808eeeb897838
Aucker AN <aucker@qq.com>
2021-06-17 11:05:59 +0800

    Update server.js
    add route for timestamp microservice

--- server.js
+++ server.js
@@ -30,3 +30,67 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+/*
+let responseObject = {}
+
+app.get('/api/timestamp/:input', (req, res) => {
+  let input = req.params.input
+
+  if(input.includes('-')){
+      //Date String
+      responseObject['unix'] = new Date(input).getTime()
+      responseObject['utc'] = new Date(input).toUTCString()
+  } else {
+      //Timestamp
+      input = parseInt(input)
+      responseObject['unix'] = new Date(input).getTime()
+      responseObject['utc'] = new Date(input).toUTCString()
+  }
+
+  if(!responseObject['unix'] || !responseObject['utc']) {
+    res.json({error: 'Invalid Date'})
+  }
+
+  res.json(responseObject)
+})
+
+app.get('/api/timestamp', (req, res) => {
+  responseObject['unix'] = new Date().getTime()
+  responseObject['utc'] = new Date().toUTCString()
+  res.json(responseObject)
+})
+*/
+
+let responseObject = {}
+
+app.get("/api/:input", (request, response) => {
+  let input = request.params.input
+  
+  if(input.includes('-') || input.includes(' ')){
+    //* Date String *
+    responseObject['unix'] = new Date(input).getTime()
+    responseObject['utc'] = new Date(input).toUTCString()
+  }else{
+    //* Timestamp *
+    input = parseInt(input)
+    
+    responseObject['unix'] = new Date(input).getTime()
+    responseObject['utc'] = new Date(input).toUTCString()
+  }
+  
+  if(!responseObject['unix'] || !responseObject['utc']){
+    response.json({error: "Invalid Date"})
+  }
+  
+  
+  response.json(responseObject)
+})
+
+app.get("/api", (request, response) => {
+  responseObject['unix'] = new Date().getTime()
+  responseObject['utc'] = new Date().toUTCString()
+  
+  response.json(responseObject)
+})
